21c7f962ce7266da0ceacc901a01f99796000130
nikki2606 <nikitamj2606@gmail.com>
2024-06-22 19:35:25 -0400

    Set matrix zeroes Hashset approach

--- 73. Set Matrix Zeroes/matrixZeroes.py
+++ 73. Set Matrix Zeroes/matrixZeroes.py
@@ -1,23 +1,23 @@
+# Time: O(n^2) Space: O(n)
 class Solution:
     def setZeroes(self, matrix: List[List[int]]) -> None:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        # Time :(n^2)
-        copyMatrix = []
+        row = set()
+        col = set()
+
         for i in range(len(matrix)):
-            copyMatrix.append([])
             for j in range(len(matrix[0])):
-                copyMatrix[i].append(matrix[i][j])
-                
-        def setRowColumnZero(r, c):
-            for i in range(len(matrix[0])):
-                matrix[r][i] = 0
-            
-            for j in range(len(matrix)):
-                matrix[j][c] = 0
+                if matrix[i][j] == 0:
+                    row.add(i)
+                    col.add(j)
         
-        for r in range(len(copyMatrix)):
-            for c in range(len(copyMatrix[0])):
-                if copyMatrix[r][c] == 0:
-                    setRowColumnZero(r,c)
+        for r in row:
+            for j in range(len(matrix[0])):
+                matrix[r][j] = 0
+        
+        for c in col:
+            for i in range(len(matrix)):
+                matrix[i][c] = 0
+                
