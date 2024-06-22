6cdff087a78c6f74e56ea7b3c72cc24bdf05bb7a
nikki2606 <nikitamj2606@gmail.com>
2024-06-22 19:32:26 -0400

    Valid anagram countvec approach

--- 242. Valid Anagram/validAnagram.py
+++ 242. Valid Anagram/validAnagram.py
@@ -1,19 +1,18 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        cvecS = [0]*26
-        cvecT = [0]*26
+        countVecS = [0]*26
+        countVecT = [0]*26
+
+        for c in s:
+            countVecS[ord(c)-ord('a')] += 1
         
-        if len(s) != len(t):
-            return False
-        
-        for i in range(len(s)):
-            cvecS[ord(s[i])-ord('a')] += 1
-            cvecT[ord(t[i])-ord('a')] += 1
-        
-        if cvecS == cvecT:
+        for c in t:
+            countVecT[ord(c)-ord('a')] += 1
+
+        if countVecS == countVecT:
             return True
         return False
-
+    
 ### Notes
 # Time: O(n)
 # Space: O(1)
