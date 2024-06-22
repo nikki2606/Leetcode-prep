ddf223b24466bfd0ea2e4aa243c1a01a28d96acc
nikki2606 <nikitamj2606@gmail.com>
2024-06-22 19:52:17 -0400

    Hashset approach contains duplicate prob

--- 217. Contains Duplicate/containsDuplicate.py
+++ 217. Contains Duplicate/containsDuplicate.py
@@ -1,13 +1,7 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        ele = set()
-        for num in nums:
-            if num in ele:
-                return True
-            
-            ele.add(num)
-        return False
-
+        return len(nums) != len(set(nums))
+    
 ### Notes
 # Time: O(n)
 # Space: O(n)
