eae57f828086cfdb48498bf7397fdfddd2fbf59b
nikki2606 <nikitamj2606@gmail.com>
2024-12-18 23:59:10 -0500

    235

---
+++ 235. Lowest Common Ancestor of a Binary Search Tree/sol.py
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        parent_val = root.val
+        p_val = p.val
+        q_val = q.val
+        if parent_val > p_val and parent_val > q_val:
+            return self.lowestCommonAncestor(root.left, p, q)
+        elif parent_val < p_val and parent_val < q_val:
+            return self.lowestCommonAncestor(root.right, p, q)
+        else:
+            return root
+        
