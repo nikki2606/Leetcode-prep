42a8ec399c88523c4ec81f28ae17f832a8d2f2c7
nikki2606 <nikitamj2606@gmail.com>
2024-12-23 00:01:37 -0500

    125

--- 125. Valid Palindrome/validPalindrome.py
+++ 125. Valid Palindrome/validPalindrome.py
@@ -1,24 +1,20 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
+        if not s:
+            return True
+        left, right = 0, len(s)-1
         s = s.lower()
-        left = 0
-        right = len(s)-1
         while left <= right:
             if not s[left].isalnum():
                 left += 1
+                continue
             
             if not s[right].isalnum():
                 right -= 1
+                continue
             
-            if left < len(s) and right >= 0:
-                if s[left].isalnum() and s[right].isalnum():
-                    if s[left] != s[right]:
-                        return False
-                    left += 1
-                    right -= 1
-
+            if s[left] != s[right]:
+                return False
+            left += 1
+            right -= 1
         return True
-
-### Notes
-# Time: O(n)
-# Space: O(1)
