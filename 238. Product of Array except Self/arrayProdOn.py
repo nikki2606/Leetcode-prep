c3accdd1714d4eacc2ed94c4bd150b2e6750981a
nikki2606 <nikitamj2606@gmail.com>
2024-12-24 17:10:22 -0500

    238

--- 238. Product of Array except Self/arrayProdOn.py
+++ 238. Product of Array except Self/arrayProdOn.py
@@ -1,20 +1,21 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
+        '''
+        nums = [1,2,3,4]
+        left_mul = [1,1,2,6]
+        right_mul = [24,12,4,1]
+        res = [24,12,8,6]
+        '''
         n = len(nums)
-        An = [1]*n
-        Bn = [1]*n
-        res = []
-        
+        left_mul = [1]*n
+        right_mul = [1]*n
+        res = [1]*n
         for i in range(1,n):
-            An[i] = An[i-1]*nums[i-1]
-            
-        for i in range(n-2,-1,-1):
-            Bn[i] = Bn[i+1]*nums[i+1]
-            
-        for i in range(n):
-            res.append(An[i]*Bn[i])
-        return res
+            left_mul[i] = left_mul[i-1]*nums[i-1]
+        
+        for j in range(n-2,-1,-1):
+            right_mul[j] = right_mul[j+1]*nums[j+1]
 
-### Notes
-# Time: O(n)
-# Space: O(n)
+        for i in range(n):
+            res[i] = left_mul[i]*right_mul[i]
+        return res
