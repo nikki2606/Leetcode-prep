99769693bb8c11a38988cfb8caf008ccd600c157
nikki2606 <nikitamj2606@gmail.com>
2024-12-25 17:03:42 -0500

    881

--- 881. Boats to Save People/boats.py
+++ 881. Boats to Save People/boats.py
@@ -1,13 +1,16 @@
-# Time: O(nlogn) Space: O(1)
 class Solution:
     def numRescueBoats(self, people: List[int], limit: int) -> int:
-        people = sorted(people)
-        l , r = 0, len(people)-1
-        res = 0
+        people.sort()
+        left, right = 0, len(people)-1
 
-        while l <= r:
-            res += 1
-            if people[l] + people[r] <= limit:
-                l += 1
-            r -= 1
-        return res
+        boat = 0
+        while left <= right:
+            summ = people[left] + people[right]
+            if summ > limit: # 1 person per boat
+                right -= 1
+                boat += 1
+            else:
+                left += 1
+                right -= 1
+                boat += 1
+        return boat
