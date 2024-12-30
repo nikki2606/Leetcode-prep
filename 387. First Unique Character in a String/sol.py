3f926051e3ca57b65c5ac51853684bbe37df42c0
nikki2606 <nikitamj2606@gmail.com>
2024-12-30 13:41:10 -0500

    387

--- 387. First Unique Character in a String/sol.py
+++ 387. First Unique Character in a String/sol.py
@@ -1,17 +1,9 @@
-# Time: O(n) Space: O(n)
-from collections import defaultdict
+from collections import Counter
 class Solution:
     def firstUniqChar(self, s: str) -> int:
-        charCount = defaultdict(int)
-        for c in s:
-            charCount[c] += 1
-        
-        index = -1
-        for c, freq in charCount.items():
+        charcount = Counter(s)
+        res = float('inf')
+        for char, freq in charcount.items():
             if freq == 1:
-                if index == -1:
-                    index = s.index(c)
-                else:
-                    index = min(index, s.index(c))
-        
-        return index
+                res = min(res,s.index(char))
+        return res if res != float('inf') else -1
