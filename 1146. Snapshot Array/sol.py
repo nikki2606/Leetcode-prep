2df974d6fe4feacaddfbc115fd386cddf2c4e27e
nikki2606 <nikitamj2606@gmail.com>
2025-01-02 20:42:08 -0500

    1146

--- 1146. Snapshot Array/sol.py
+++ 1146. Snapshot Array/sol.py
@@ -1,27 +1,27 @@
 class SnapshotArray:
 
-    def __init__(self, n: int):
-        self.snapshot_id = 0
-        self.arr = [[[-1, 0]] for _ in range(n)]
+    def __init__(self, length: int):
+        self.arr = [[[0,0]] for i in range(length)]
+        self.snap_id = 0
 
     def set(self, index: int, val: int) -> None:
-        self.arr[index].append([self.snapshot_id, val])
+        self.arr[index].append([self.snap_id, val])
 
     def snap(self) -> int:
-        self.snapshot_id += 1
-        return self.snapshot_id - 1
+        self.snap_id += 1
+        return self.snap_id-1
 
     def get(self, index: int, snap_id: int) -> int:
         left, right = 0, len(self.arr[index])-1
-        pos = -1
+        res = 0
         while left <= right:
             mid = (left+right)//2
             if self.arr[index][mid][0] <= snap_id:
-                pos = mid
+                res = self.arr[index][mid][1]
                 left = mid + 1
             else:
                 right = mid - 1
-        return self.arr[index][pos][1]
+        return res
 
 
 # Your SnapshotArray object will be instantiated and called as such:
