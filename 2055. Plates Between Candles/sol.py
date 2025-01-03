49bee67ac6004b06500b4f87b25a8f7d525382c5
nikki2606 <nikitamj2606@gmail.com>
2025-01-03 17:33:46 -0500

    2055

--- 2055. Plates Between Candles/sol.py
+++ 2055. Plates Between Candles/sol.py
@@ -1,34 +1,39 @@
 class Solution:
     def platesBetweenCandles(self, s: str, queries: List[List[int]]) -> List[int]:
-        candles = []
+        candles = [i for i in range(len(s)) if s[i] == "|"]
         res = []
-        for i in range(len(s)):
-            if s[i] == "|":
-                candles.append(i)
 
         for qleft, qright in queries:
-            left_pos, right_pos = -1, -1
-
-            left, right = 0, len(candles)-1
-            while left <= right:
-                mid = (left+right)//2
-                if candles[mid] >= qleft:
-                    right = mid - 1
-                    left_pos = mid
-                else:
-                    left = mid + 1
-            
-            left, right = 0, len(candles)-1
-            while left <= right:
-                mid = (left+right)//2
-                if candles[mid] <= qright:
-                    left = mid + 1
-                    right_pos = mid
-                else:
-                    right = mid - 1
-            
-            if left_pos != -1 and right_pos != -1 and left_pos < right_pos:
-                res.append((candles[right_pos]-candles[left_pos])-(right_pos - left_pos))
-            else:
+            if qleft == qright:
                 res.append(0)
+                continue
+            lcandle, rcandle = self.nearest_right(qleft, candles), self.nearest_left(qright, candles)
+            candles_in_btw = rcandle - lcandle - 1 if rcandle > lcandle else 0
+            plates = candles[rcandle] - candles[lcandle] - 1 - candles_in_btw if rcandle > lcandle else 0
+            res.append(plates)
+        
         return res
+    
+    def nearest_right(self, x, candles):
+        left, right = 0, len(candles)-1
+        out = 0
+        while left <= right:
+            mid = (left+right)//2
+            if candles[mid] >= x:
+                out = mid
+                right = mid - 1
+            else:
+                left = mid + 1
+        return out
+
+    def nearest_left(self, x, candles):
+        left, right = 0, len(candles)-1
+        out = len(candles)-1
+        while left <= right:
+            mid = (left+right)//2
+            if candles[mid] <= x:
+                out = mid
+                left = mid + 1
+            else:
+                right = mid - 1
+        return out
