e4c36779de791fc05f9632026c35ebc06756965c
nikki2606 <nikitamj2606@gmail.com>
2025-01-06 00:46:32 -0500

    570

---
+++ 570. Dot Product of Two Sparse Vectors/sol.py
@@ -0,0 +1,17 @@
+class SparseVector:
+    def __init__(self, nums: List[int]):
+        self.nums = nums
+
+    # Return the dotProduct of two sparse vectors
+    def dotProduct(self, vec: 'SparseVector') -> int:
+        n = len(self.nums)
+        res = 0
+        for i in range(n):
+            res += self.nums[i]*vec.nums[i]
+        return res
+        
+
+# Your SparseVector object will be instantiated and called as such:
+# v1 = SparseVector(nums1)
+# v2 = SparseVector(nums2)
+# ans = v1.dotProduct(v2)
