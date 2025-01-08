1047f150d6ba3d53d8f17f870de2e9c294d22369
nikki2606 <nikitamj2606@gmail.com>
2025-01-08 17:41:02 -0500

    221

--- 221. Maximal Square/sol.py
+++ 221. Maximal Square/sol.py
@@ -2,22 +2,18 @@ class Solution:
     def maximalSquare(self, matrix: List[List[str]]) -> int:
         n = len(matrix)
         m = len(matrix[0])
-        dp = [[0 for j in range(m)] for i in range(n)]
+        dp = [[0 for _ in range(m)] for _ in range(n)]
+
+        for i in range(n):
+            for j in range(m):
+                if matrix[i][j] == "1":
+                    dp[i][j] += 1
+                    if i>0 and j>0:
+                        dp[i][j] += min(dp[i-1][j], dp[i][j-1], dp[i-1][j-1])
+        
         sqlen = 0
         for i in range(n):
-            dp[i][0] = int(matrix[i][0])
-            sqlen = max(sqlen, dp[i][0])
-        
-        for j in range(m):
-            dp[0][j] = int(matrix[0][j])
-            sqlen = max(sqlen, dp[0][j])
-        
-        for i in range(1,n):
-            for j in range(1,m):
-                if matrix[i][j] == "1":
-                    dp[i][j] = 1 + min(dp[i][j-1], dp[i-1][j], dp[i-1][j-1])
-                    sqlen = max(sqlen, dp[i][j])
+            for j in range(m):
+                sqlen = max(sqlen, dp[i][j])
         
         return sqlen*sqlen
-
-# Time: O(n*m)
