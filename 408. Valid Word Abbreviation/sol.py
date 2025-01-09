7a4dd14241c74127e49b2f88758373ad59ed7b04
nikki2606 <nikitamj2606@gmail.com>
2025-01-09 15:17:42 -0500

    408

--- 408. Valid Word Abbreviation/sol.py
+++ 408. Valid Word Abbreviation/sol.py
@@ -1,24 +1,28 @@
 class Solution:
     def validWordAbbreviation(self, word: str, abbr: str) -> bool:
+        if not word and not abbr:
+            return True
+
+        if not word or not abbr:
+            return False
+
         i, j = 0, 0
-        n, m = len(word), len(abbr) 
+        n, m = len(word), len(abbr)
         while i<n and j<m:
             if abbr[j].isdigit():
                 if abbr[j] == "0":
                     return False
-                
-                num = 0
+
+                cursum = 0
                 while j < m and abbr[j].isdigit():
-                        num = num*10 + int(abbr[j])
-                        j += 1
-                i += num
+                    cursum = cursum*10 + int(abbr[j])
+                    j += 1
+                i += cursum
             else:
-                if i >= n:
-                    return False
                 if word[i] != abbr[j]:
                     return False
-                    
-                i += 1
                 j += 1
-                
+                i += 1
+               
+        
         return i == n and j == m
