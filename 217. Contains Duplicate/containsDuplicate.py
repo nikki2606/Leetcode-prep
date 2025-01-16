a316bfc6eb63a9440af2f6ef17ad26c01b79c74a
nikki2606 <nikitamj2606@gmail.com>
2025-01-15 19:38:36 -0500

    207

--- 217. Contains Duplicate/containsDuplicate.py
+++ 217. Contains Duplicate/containsDuplicate.py
@@ -1,7 +1,3 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        return len(nums) != len(set(nums))
-    
-### Notes
-# Time: O(n)
-# Space: O(n)
+        return len(set(nums)) != len(nums)
