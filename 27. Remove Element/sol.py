a575c552f96f046a250262b9308876b2ac40f9b2
nikki2606 <nikitamj2606@gmail.com>
2025-01-15 23:06:01 -0500

    27

--- 27. Remove Element/sol.py
+++ 27. Remove Element/sol.py
@@ -1,14 +1,11 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        val_freq = 0
-        n = len(nums)
+        if not nums:
+            return 0
+
         i = 0
-        right = n-1
-        while i < (n-val_freq):
-            if nums[i] == val:
-                nums[i], nums[right] = nums[right], nums[i]
-                right -= 1
-                val_freq += 1
-            else:
+        for j in range(len(nums)):
+            if nums[j] != val:
+                nums[i] = nums[j]
                 i += 1
-        return n-val_freq
+        return i
