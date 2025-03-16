b3ce9fa3322f6c76d1b19afe24140338f27d0b77
nikki2606 <nikitamj2606@gmail.com>
2025-03-16 14:03:42 -0400

    neetcode array

--- 14. Longest Common Prefix/sol.py
+++ 14. Longest Common Prefix/sol.py
@@ -1,8 +1,35 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.freq = 0
+
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
+        if not strs:
+            return ""
+
+        n = len(strs)
+        
+        data = TrieNode()
+        for s in strs:
+            node = data
+            for char in s:
+                if char not in node.children:
+                    node.children[char] = TrieNode()
+                node = node.children[char]
+                node.freq += 1
+        
+        node = data
         res = ""
-        for i in range(len(strs[0])):
-            for s in strs:
-                if i == len(s) or s[i] != strs[0][i]:
-                    return s[:i]
-        return strs[0]
+        while node:
+            if len(node.children) != 1:
+                return res
+            char = next(iter(node.children))
+            node = node.children[char]
+            if node.freq != n:
+                return res
+            res = res + char
+        return ""
+    
+    # Time: O(n*m)
+    # Space: O(n*m)
