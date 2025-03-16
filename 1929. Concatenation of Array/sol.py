929498a8328fc0f60537c2a50d544d12774bcfca
nikki2606 <nikitamj2606@gmail.com>
2025-03-16 12:19:23 -0400

    neetcode array

--- 1929. Concatenation of Array/sol.py
+++ 1929. Concatenation of Array/sol.py
@@ -1,5 +1,8 @@
 class Solution:
     def getConcatenation(self, nums: List[int]) -> List[int]:
+        if not nums:
+            return []
+
         n = len(nums)
         res = [0]*(2*n)
         for i in range(n):
