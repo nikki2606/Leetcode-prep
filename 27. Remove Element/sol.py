c1b0af774e0c073cacdd594c672422e268a82066
nikki2606 <nikitamj2606@gmail.com>
2025-03-16 14:35:02 -0400

    neetcode array

--- 27. Remove Element/sol.py
+++ 27. Remove Element/sol.py
@@ -3,9 +3,12 @@ class Solution:
         if not nums:
             return 0
 
-        i = 0
-        for j in range(len(nums)):
-            if nums[j] != val:
-                nums[i] = nums[j]
-                i += 1
-        return i
+        l = 0
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[l] = nums[i]
+                l += 1
+        return l
+    
+    # Time: O(n)
+    # Space: O(1)
