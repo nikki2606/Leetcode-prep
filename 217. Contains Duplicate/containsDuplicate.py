cf9b65e3ae4aaab2a5904a067eaa5df5429bf461
nikki2606 <nikitamj2606@gmail.com>
2025-04-12 22:05:55 +0400

    Neetcode Arrays

--- 217. Contains Duplicate/containsDuplicate.py
+++ 217. Contains Duplicate/containsDuplicate.py
@@ -1,7 +1,5 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        if not nums:
-            return False
         return len(nums) != len(set(nums))
 
 # Time: O(n)
